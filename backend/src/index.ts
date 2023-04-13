8e0e04a6f1349eb7b2fa0db6bc8a7f46195cfd16
Yuji <yujiricardo@gmail.com>
2023-04-12 21:23:00 -0300

    chore: add ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,9 @@
-import express from 'express'
+import express, { Express, Request, Response } from 'express';
 import pg from 'pg'
 import cors from 'cors'
 import { faker } from '@faker-js/faker';
 
-const app = express()
+const app: Express = express()
 
 const pool = new pg.Pool({
   host: 'localhost',
@@ -17,14 +17,16 @@ app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/comments', async (req, res) => {
-  const {page, size} = req.query
+type CommentsQueryParams = { page: number, size: number }
+
+app.get('/comments', async (req: Request, res: Response) => {
+  const { page, size } = req.query as unknown as CommentsQueryParams
 
   try {
-    if(page <= 0) {
+    if (page <= 0) {
       throw Error('page must be bigger than 0')
     }
-  
+
     const { rows } = await pool.query(`
         SELECT comment_text, users.username FROM comments c
         JOIN users ON c.username_id = users.user_id
@@ -33,10 +35,10 @@ app.get('/comments', async (req, res) => {
         OFFSET (($1 - 1) * $2);
   `, [page, size]);
 
-  const {rows: count}  = await pool.query(`
+    const { rows: count } = await pool.query(`
     SELECT COUNT(*) FROM comments;
   `)
-  
+
     res.status(200).json({
       total: count[0].count,
       currentPage: page,
@@ -54,7 +56,7 @@ app.get('/comments', async (req, res) => {
 
 })
 
-app.post('/comment', async (req, res) => {
+app.post('/comment', async (req: Request, res: Response) => {
   const {
     username,
     commentText
@@ -63,14 +65,14 @@ app.post('/comment', async (req, res) => {
   try {
     const user = await pool.query(`SELECT * FROM users WHERE username = $1`, [username])
 
-    if(user.rows.length <= 0) {
+    if (user.rows.length <= 0) {
       console.log('não achou usuário')
-      await pool.query(`INSERT INTO users (username) VALUES($1)`, [username]) 
+      await pool.query(`INSERT INTO users (username) VALUES($1)`, [username])
     }
-    const {rows} = await pool.query(`SELECT user_id FROM users WHERE username = $1`, [username])
+    const { rows } = await pool.query(`SELECT user_id FROM users WHERE username = $1`, [username])
     await pool.query('INSERT INTO comments (username, comment_text, username_id) VALUES ($1, $2, $3);',
-     [username,
-      commentText, rows[0].username_id]);
+      [username,
+        commentText, rows[0].username_id]);
 
     res.json({
       statusCode: 201,
@@ -79,8 +81,8 @@ app.post('/comment', async (req, res) => {
         username,
         commentText
       }
-    })  
-    
+    })
+
   } catch (error) {
     console.log(error)
     res.json({
@@ -90,26 +92,26 @@ app.post('/comment', async (req, res) => {
   }
 })
 
-app.post('/seed', async (req, res) => {
+app.post('/seed', async (req: Request, res: Response) => {
 
-  const {amount} = req.query
+  const { amount } = req.query
 
   try {
-    for(let i = 0; i < amount; i++) {
+    for (let i = 0; i < Number(amount); i++) {
 
       pool.query(`BEGIN`)
       const username = faker.name.fullName()
       const user = await pool.query(`SELECT * FROM users WHERE username = $1`, [username])
 
-      if(user.rows.length <= 0) {
-        await pool.query(`INSERT INTO users (username) VALUES($1)`, [username]) 
+      if (user.rows.length <= 0) {
+        await pool.query(`INSERT INTO users (username) VALUES($1)`, [username])
       }
 
-      const userId =  await pool.query('SELECT username, user_id FROM users WHERE username = $1',
+      const userId = await pool.query('SELECT username, user_id FROM users WHERE username = $1',
         [username]);
-      
+
       await pool.query('INSERT INTO comments (comment_text, username_id) VALUES ($1, $2);',
-      [faker.lorem.paragraph(), userId.rows[0].user_id]);
+        [faker.lorem.paragraph(), userId.rows[0].user_id]);
 
     }
 
@@ -118,7 +120,7 @@ app.post('/seed', async (req, res) => {
       statusCode: 201,
       text: 'criado!',
       amount: `${amount}`
-    }) 
+    })
   } catch (error) {
     pool.query(`ROLLBACK`)
 
